08c473dd8cf80e51b0706f73925223424ae0c078
Vishnu Kumar <97397206+vishnu-kumar-k@users.noreply.github.com>
2023-06-07 18:41:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { Container, Button, Row, Col, Form } from "react-bootstrap";
 import { ToastContainer, toast } from "react-toastify";
 
-const socket = io("http://localhost:8000");
+const socket = io("http://43.205.110.43:8000");
 
 const App = () => {
   const [joinWithNumber, setJoinWithNumber] = useState(0);
