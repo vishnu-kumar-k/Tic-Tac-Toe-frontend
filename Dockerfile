65c6032d85f3dd8c396c8415ce73c69071ba7e88
vishnu-kumar-k <vishnuk55265@gmail.com>
2023-06-07 17:42:57 +0530

    last

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use an official Node.js runtime as the base image
+FROM node:14
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the container
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the entire project to the container
+COPY . .
+
+# Build the React app
+RUN npm run build
+
+# Expose the container's port
+EXPOSE 3000
+
+# Define the command to run the app when the container starts
+CMD ["npm", "start"]
--- src/App.js
+++ src/App.js
@@ -1,25 +1,272 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import io from "socket.io-client";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import { Container, Button, Row, Col, Form } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
 
-function App() {
+const socket = io("http://localhost:8000");
+
+const App = () => {
+  const [joinWithNumber, setJoinWithNumber] = useState(0);
+  const [number, setNumber] = useState("");
+  const [flag, setFlag] = useState(false);
+  const [message, setMessage] = useState("");
+  const [temp, setTemp] = useState();
+  const clickRef = useRef(true);
+  const [game, setGame] = useState(false);
+  const [player, setPlayer] = useState();
+  const [mymessage, setMymessage] = useState([]);
+  const [friendmessage, setFriendmessage] = useState([]);
+  const [messaget, setMessaget] = useState("");
+  const [gameBoard, setGameBoard] = useState([
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ]);
+
+  const vRef = useRef();
+  const setVariable = (value) => {
+    vRef.current = value;
+  };
+
+  useEffect(() => {
+    if (game) {
+      const handleBeforeUnload = (event) => {
+        event.preventDefault();
+        event.returnValue = "Are you Confirm?"; // Display a confirmation dialog
+      };
+
+      const handleUnload = (e) => {
+        if (window.confirm("Are you sure you want to leave this page?")) {
+          console.log("dddsf");
+        } else {
+          // Stay on the current page
+          e.preventDefault();
+        }
+      };
+
+      window.addEventListener("beforeunload", handleBeforeUnload);
+      window.addEventListener("unload", handleUnload);
+
+      return () => {
+        window.removeEventListener("beforeunload", handleBeforeUnload);
+        window.removeEventListener("unload", handleUnload);
+      };
+    }
+  }, [game]);
+
+  useEffect(() => {
+    socket.on("leaveGame", () => {
+      setMessage("You have won the Game");
+      toast.success("Player Has left");
+    });
+
+    socket.on("waiting", () => {
+      setMessage("Waiting for the Player");
+    });
+
+    socket.on("Error", () => {
+      toast.warning("No Game Created");
+    });
+
+    socket.on("updateMessage", (m) => {
+      setFriendmessage((prevMessage) => [...prevMessage, m]);
+    });
+
+    socket.on("startGame", async (temp, con, p) => {
+      await setTemp(temp);
+      await setPlayer(p);
+      await setGame(true);
+      setVariable(con);
+
+      if (con === "O") {
+        clickRef.current = false;
+      }
+
+      setMessage("Game Started");
+      setFlag(true);
+    });
+
+    socket.on("updateGame", (newGameBoard) => {
+      setGameBoard(newGameBoard);
+      clickRef.current = true;
+    });
+
+    console.log(vRef.current);
+
+    socket.on("gameWinner", (t) => {
+      clickRef.current = false;
+      setGame(false);
+      console.log();
+
+      if (t === vRef.current) {
+        console.log("Won");
+        setMessage("You have Won");
+      } else if (t === "draw") {
+        setMessage("Match has been Drawn");
+      } else {
+        setMessage("You have Lost");
+      }
+    });
+  }, []);
+
+  const handleJoinWithNumber = () => {
+    setJoinWithNumber(1);
+  };
+
+  const handleJoin = () => {
+    setMessage("Loading.............");
+    socket.emit("joinWithEnteredNumber", number);
+  };
+
+  function generateRandomNumber() {
+    const min = 100000; // Minimum value (inclusive)
+    const max = 999999; // Maximum value (inclusive)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const handleRandomJoin = (e) => {
+    e.preventDefault();
+    setMessage("Waiting for Another Player");
+    setJoinWithNumber(10);
+    socket.emit("joinRandom");
+  };
+
+  const handleGenerateNumber = (e) => {
+    e.preventDefault();
+    setJoinWithNumber(3);
+    const randomNumber = generateRandomNumber();
+    setMessage(`Enter this number ${randomNumber}`);
+    socket.emit("joinWithNumber", randomNumber);
+  };
+
+  const handleCellClick = (rowIndex, colIndex) => {
+    console.log(clickRef.current);
+
+    if (gameBoard[rowIndex][colIndex] === "" && clickRef.current) {
+      // Make sure the cell is empty and the game has started
+      const newGameBoard = [...gameBoard];
+      newGameBoard[rowIndex][colIndex] = vRef.current; // Set the clicked cell to 'X' for the example
+      setGameBoard(newGameBoard);
+      clickRef.current = false;
+
+      // Emit an event to update the game board on the server and notify the other player
+      socket.emit("updateGameBoard", newGameBoard, temp, player);
+    }
+  };
+
+  const handlemessage = (e) => {
+    e.preventDefault();
+    socket.emit("Message", temp, player, messaget);
+    setMymessage((prevMessage) => [...prevMessage, messaget]);
+    setMessaget("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ToastContainer />
+      <h1 className="d-flex justify-content-center tittle">Tic Tac Toe</h1>
+      {!flag ? (
+        <>
+          {joinWithNumber === 0 ? (
+            <div className="d-flex justify-content-around">
+              <Button onClick={handleJoinWithNumber}>Enter with Number</Button>
+              <Button onClick={handleRandomJoin}>Join Random</Button>
+              <Button onClick={handleGenerateNumber}>Generate Number</Button>
+              </div>
+          ) : null}
+
+          {joinWithNumber === 1 ? (
+            <>
+              <Form.Group controlId="numberInput" className="d-flex justify-content-around">
+                <Form.Label>Enter the same number Display on Friend</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={number}
+                  onChange={(e) => setNumber(e.target.value)}
+                />
+              </Form.Group>
+              <div className="d-flex justify-content-around">
+              <Button
+              className="d-flex justify-content-center"
+                variant="primary"
+                style={{marginTop:"1em"}}
+                onClick={handleJoin}
+                disabled={!number || number.length < 4}
+              >
+                Join
+              </Button>
+              </div>
+            </>
+          ) : (
+            <h1>{message}</h1>
+          )}
+        </>
+      ) : (
+        <div >
+          <h3 className="d-flex justify-content-around">{message}</h3>
+          {game?(<>
+          {clickRef.current?(<h6 className="d-flex justify-content-around">Your Turn</h6>):(<h6 className="d-flex justify-content-around">Waiting for Friend</h6>)}
+          </>):null}
+          <div className="game-board">
+            {gameBoard.map((row, rowIndex) => (
+              <div key={rowIndex} className="rows">
+                {row.map((cell, colIndex) => (
+                  <div
+                    key={colIndex}
+                    className="cells"
+                    onClick={() => handleCellClick(rowIndex, colIndex)}
+                  >
+                    {cell}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+          <div className="message">
+            <Row>
+              <Col md={2}></Col>
+              <Col md={4}>
+                <h4>My Chat</h4>
+                {mymessage.map((message, index) => (
+                  <div key={index}>{message}</div>
+                ))}
+              </Col>
+              <Col md={4}>
+                <h4>Player Chat</h4>
+                {friendmessage.map((message, index) => (
+                  <div
+                    key={index}
+                    className={index % 2 === 0 ? "left" : "right"}
+                  >
+                   {index%2===0?(<>{message}</>):(null)} 
+                  </div>
+                ))}
+              </Col>
+              <Col md={2}></Col>
+              <Form.Group controlId="messageInput" className="w-100">
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  value={messaget}
+                  onChange={(e) => setMessaget(e.target.value)}
+                />
+              </Form.Group>
+              <Button
+                variant="primary"
+                onClick={handlemessage}
+                disabled={!messaget}
+              >
+                Send
+              </Button>
+            </Row>
+          </div>
+        </div>
+      )}
+    </Container>
   );
-}
+};
 
 export default App;
